a6af5875d00a984cd200bd5a51db052055f7fecb
Ernani Gomes <lic.fisica.cederj@gmail.com>
2022-10-29 05:47:59 -0300

    Adicionado arquivos com texto dos desafios

---
+++ paginas-interativas-com-javascript/primeiros-passos-com-javascript/praticando-variaveis-operadores-estruturas-condicionais/resolucao-desafio-imc-e-condicao.js
@@ -0,0 +1,16 @@
+/*
+ * Indice de Massa Corporal (IMC)
+ *
+ * Formula do IMC:
+ * IMC = peso / (altura * altura)
+ *
+ * Crie um programa que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+ *
+ * IMC em adultos Condição:
+ * - Abaixo de 18.5 Abaixo do peso;
+ * - Entre 18.5 e 25 Peso normal;
+ * - Entre 25 e 30 Acima do peso;
+ * - Entre 30 e 40 Obeso;
+ * - Acima de 40 Obsesidade Grave;
+ *
+ */
---
+++ paginas-interativas-com-javascript/primeiros-passos-com-javascript/praticando-variaveis-operadores-estruturas-condicionais/resolucao-desafio-media-e-classificacao.js
@@ -0,0 +1,12 @@
+/*
+ * Crie um programa que passadas 3 notas de um aluno calcule e imprima sua média e classificação
+ * conforme a tabela abaixo:
+ *
+ * Média = (nota 1 + nota 2 + nota 3) / 3;
+ *
+ * Classificação:
+ * - Média menor que 5, reprovação;
+ * - Média entre 5 e 7, recuperação;
+ * - Média acima de 7, passou de semestre;
+ *
+ */
