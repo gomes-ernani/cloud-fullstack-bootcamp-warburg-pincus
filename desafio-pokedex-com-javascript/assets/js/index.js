f1d60341c21cbda3a759911a560922a78c85fce6
Ernani Gomes <lic.fisica.cederj@gmail.com>
2022-11-09 04:11:38 -0300

    Ajustando formação do index.js

--- desafio-pokedex-com-javascript/assets/js/index.js
+++ desafio-pokedex-com-javascript/assets/js/index.js
@@ -10,7 +10,9 @@ function pokemonToLiConverter(pokemon) {
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
-							<ol class="types">${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}</ol>
+							<ol class="types">${pokemon.types
+                .map((type) => `<li class="type ${type}">${type}</li>`)
+                .join("")}</ol>
 								<img src="${pokemon.photo}" alt="${pokemon.name}">
 						</div>
 					</li>`;
